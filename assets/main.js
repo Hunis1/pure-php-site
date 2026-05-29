536be27ac96290ec252d9b3546da10cb74054be5
Hunis1 <156211204+Hunis1@users.noreply.github.com>
2026-05-29 16:51:12 +0300

    Sort functional for category page and mark on active taxonomy button

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('ertyerty')
+    let select = document.querySelectorAll('select');
+    if(select) {
+        select.forEach(el => {
+            let attrValue = el.getAttribute('data-selected');
+            let options = el.querySelectorAll('option');
+            if(options) {
+                options.forEach(item => {
+                    if(item.value === attrValue) {
+                        item.setAttribute('selected','')
+                    }
+                })
+            }
+        })
+    }
+
+    let pagination = document.querySelectorAll('.pagination');
+    if(pagination) {
+        pagination.forEach(el => {
+            let params = new URLSearchParams(document.location.search);
+            let pageNumber;
+            if(params) {
+                pageNumber = params.get('page');
+            }
+            let items = el.querySelectorAll('.pagination-item');
+            if(items) {
+                items.forEach(item => {
+                    if(pageNumber != undefined && item.getAttribute('data-page') === pageNumber) {
+                        item.classList.add('active');
+                    }
+                })
+            }
+        })
+    }
 });
