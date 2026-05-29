ff2fcbbf8d61f79b707c9de09745ece1ae89e836
Hunis1 <156211204+Hunis1@users.noreply.github.com>
2026-05-29 16:49:04 +0300

    Code has become shorter

--- archive-news.php
+++ archive-news.php
@@ -2,11 +2,21 @@
 require 'vendor/autoload.php';
 use Smarty\Smarty;
 $smarty = new Smarty();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
 
-$data = require_once($_SERVER['DOCUMENT_ROOT'] . '/data/news-cats-data.php');
-$smarty->assign(
-    'news_cats_array', json_decode($data)
-);
+$query = $mysqli->query('SELECT * FROM news_cats');
+$categories = [];
+foreach($query as $item) {
+    $item['posts'] = [];
+    $postsQuery = $mysqli->query('SELECT * FROM news WHERE JSON_CONTAINS(news_cats_id,"' . $item['id'] . '") ORDER BY published DESC LIMIT 3');
+    foreach($postsQuery as $row) {
+        array_push($item['posts'],$row);
+    }
+    array_push($categories,$item);
+}
 
+$smarty->assign('news_cats_array', $categories);
 $smarty->display('archive-news.tpl')
 ?>
